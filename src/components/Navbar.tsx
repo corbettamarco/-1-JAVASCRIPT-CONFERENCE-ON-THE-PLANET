22ce03a73ae47989005eace7cb1cdd5c9e72ed2e
corbettamarco <corbettamarco.it@gmail.com>
2023-01-30 13:08:26 +0100

    terza commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,16 +8,12 @@ import {
 export const Navbar = () => {
 
   return (<>
-    <Box bgColor="black" px="1em" shadow={"lg"} >
+    <Box bgColor="black" px="1em"  >
       <Flex h={16} alignItems={"center"} >
-        <HStack spacing="1em" alignItems={"center"}>
+        <HStack my="1em" spacing="1em" alignItems={"center"}>
           <Image src="https://assets.jsworldconference.com/logo_jsworld_ad7f539826.svg" h={14}></Image>
           <Text display={{ base: "none", sm: "flex" }} textColor="conf.red.500" fontWeight={"bold"} >JSWORLD CONFERENCE 2023</Text>
           <Box position="absolute" right="1em" >
-            <HStack >
-              <Box display={{ base: "none", md: "flex" }}>
-              </Box>
-            </HStack>
           </Box>
         </HStack>
       </Flex>

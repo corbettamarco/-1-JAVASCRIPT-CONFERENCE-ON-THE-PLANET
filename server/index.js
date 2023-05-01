8fc79756ecf336d91027e0a10d7a47434b5db56e
corbettamarco <corbettamarco.it@gmail.com>
2023-05-02 00:55:19 +0200

    test

--- server/index.js
+++ server/index.js
@@ -9,19 +9,13 @@ const middlewares = jsonServer.defaults();
 app.use('/api', middlewares);
 app.use('/api', router);
 
-if (process.env.NODE_ENV === "production") {
+// Serve React app from the 'build' folder
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
-  app.use(express.static('client/build'))  // set static folder 
-  
-  app.get('*', (req, res)=> {     
-    res.sendFile(path.resolve(__dirname, 'client', 'build',         
-                  'index.html' )); 
-  })
-  app.get('*', (req, res)=> {     
-    res.sendFile(path.resolve(__dirname, 'client', 'build',         
-                  'index.html' )); 
-  })
-}
+// Catch-all route to serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 
